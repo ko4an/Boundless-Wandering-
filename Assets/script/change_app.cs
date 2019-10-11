4dfcb38b4fa1a90047b423ef12a9a06a908e651b
ko4an <43035547+ko4an@users.noreply.github.com>
2019-10-11 17:53:39 +0300

    version(0.4.2)

---
+++ Assets/script/change_app.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_app : MonoBehaviour
+{
+    private Sprite[] sprites;
+    private SpriteRenderer change;
+    private string path = "player";
+
+    void Start()
+    {
+        change = GetComponent<SpriteRenderer>();
+        sprites = Resources.LoadAll<Sprite>(path);
+        Debug.Log(sprites);
+        change.sprite = sprites[2];
+    }
+
+    void Update()
+    {
+        
+    }
+}
